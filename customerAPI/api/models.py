c71072016b0e9fd7b1a7f0a1bb1047c5395d88c6
Yuvan <yuviyuvan15@gmail.com>
2023-11-05 00:40:32 +0530

    second commit

--- customerAPI/api/models.py
+++ customerAPI/api/models.py
@@ -4,10 +4,11 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True,default="")
+    password=models.CharField(max_length=255,default="")
     phoneNumber = models.CharField(max_length=255)
     wishlist = JSONField(default=[{}])
     transactions = JSONField(default=[{}])
-    # shops_saved=JSONField(default=[{}])
+    shops_saved=JSONField(default=[{}])
     def __str__(self):
         return self.name
 
--- shopAPI/api/models.py
+++ shopAPI/api/models.py
@@ -14,12 +14,11 @@ class Shop(models.Model):
     
 class Transactions(models.Model):
     id=models.AutoField(primary_key=True)
-    shopId=models.ForeignKey(Shop, on_delete=models.CASCADE)
+    shopId=models.ForeignKey(Shop, on_delete=models.CASCADE,unique=False)
     customerId=models.IntegerField(blank=False,default=0)
     createdAt = models.DateTimeField(auto_now_add=True)
     products=JSONField(default=[])
 
-    
     def __str__(self):
         return str(self.id)
     
